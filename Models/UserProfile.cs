fcd2df174ab6ae8bb4dab9227fb6a5937fc64d72
Sickle41 <sickle41@gmail.com>
2025-01-14 10:19:24 -0600

    SaveMedSched

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -7,10 +7,14 @@ public class UserProfile
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Address { get; set; }
 
     public string IdentityUserId { get; set; }
 
     public IdentityUser IdentityUser { get; set; }
+
+     public ICollection<Medication> Medications { get; set; }
+
+    // Navigation property for medication schedules
+    public ICollection<MedicationSchedule> MedicationSchedules { get; set; }
 
 }
